f8243cf506e281708e5333b800e3168d3970053f
Viktor Ganin <vganin@gmail.com>
2019-04-19 15:49:12 +0300

    Lint fixes

--- ses.py
+++ ses.py
@@ -1,52 +1,56 @@
+"""Cisco ASA vpn clients list"""
 import base64
 import json
 import sys
 import urllib2
-import pyjq
 import logging
 import time
 import re
 import ConfigParser
 from functools import wraps
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ChatAction, constants, ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, RegexHandler, BaseFilter
+from telegram import constants
+from telegram.ext import Updater, CommandHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-# Uncomment the following two lines, if you are using Python 2.7.9 or above to connect to an ASA with a self-signed certificate.
+"""
+Uncomment the following lines,
+if you are using Python 2.7.9 or above to connect to an ASA with a self-signed certificate.
+"""
 # import ssl
 # ssl._create_default_https_context = ssl._create_unverified_context
 
 # Configuration
-parser = ConfigParser.ConfigParser()
-parser.read('config.ini')
+PARSER = ConfigParser.ConfigParser()
+PARSER.read('config.ini')
 
-ipaddress = parser.get('options', 'ipaddress')
-username = parser.get('options', 'username')
-password = parser.get('options', 'password')
-port = parser.get('options', 'port')
-bot_id = parser.get('options', 'bot_id')
-LIST = parser.get('options', 'LIST')
-group1 = parser.get('options', 'group1')
-group2 = parser.get('options', 'group2')
+IPADRESS = PARSER.get('options', 'ipaddress')
+USERNAME = PARSER.get('options', 'username')
+PASSWORD = PARSER.get('options', 'password')
+PORT = PARSER.get('options', 'port')
+BOT_ID = PARSER.get('options', 'bot_id')
+LIST = PARSER.get('options', 'LIST')
+GROUP1 = PARSER.get('options', 'group1')
+GROUP2 = PARSER.get('options', 'group2')
 if len(sys.argv) > 1:
-    username = sys.argv[1]
+    USERNAME = sys.argv[1]
 if len(sys.argv) > 2:
-    password = sys.argv[2]
-# Mgmt url
-server = "https://" + ipaddress + ":" + port
-headers = {'Content-Type': 'application/json'}
+    PASSWORD = sys.argv[2]
+# Mgmt URL
+SERVER = "https://" + IPADRESS + ":" + PORT
+HEADERS = {'Content-Type': 'application/json'}
 
-api_path = "/api/cli"    # param
-url = server + api_path
-f = None
+API_PATH = "/api/cli"    # param
+URL = SERVER + API_PATH
 
 def restricted(func):
+    """Restriction function"""
     @wraps(func)
     def wrapped(bot, update):
+        """wraper"""
         user_id = update.effective_user.id
         if str(user_id) not in LIST:
             update.message.reply_text("Unauthorized access denied for {}.".format(user_id))
@@ -54,96 +58,97 @@ def restricted(func):
         return func(bot, update)
     return wrapped
 
-def help(bot, update):
-        user_id = update.message.from_user.id
-        chat_id = update.message.chat.id
-        update.message.reply_text(user_id)
+def start(bot, update):
+    """Start function"""
+    chat_id = update.message.chat.id
+    update.message.reply_text(chat_id)
 
 def get_sessions():
-    global r5
-    post_data = {
-      "commands": [
-          "show vpn-sessiondb anyconnect | include Username|Assigned IP|Group Policy"
-	  ]
-	}
-    req = urllib2.Request(url, json.dumps(post_data), headers)
-    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    req.add_header("Authorization", "Basic %s" % base64string)   
+    """Request to ASA API"""
+    post_data = {"commands":
+                 ["show vpn-sessiondb anyconnect | include Username|Assigned IP|Group Policy"]}
+    req = urllib2.Request(URL, json.dumps(post_data), HEADERS)
+    base64string = base64.encodestring('%s:%s' % (USERNAME, PASSWORD)).replace('\n', '')
+    req.add_header("Authorization", "Basic %s" % base64string)
     try:
-        f  = urllib2.urlopen(req)
-        status_code = f.getcode()
+        con = urllib2.urlopen(req)
+        status_code = con.getcode()
         print "Status code is "+str(status_code)
-        resp = f.read()
-        r = str('%s' % (resp)).replace('{"response":["' ,'').replace('"]}', '').replace('\\n', '\n')
-        r0 = re.sub(r'\nGroup Policy\s+[:]\s\w+\s+\w+\s\w+', '', r)
-        r0 = re.sub(r'[:]\s%s' % (group1), '', r0)
-        r1 = re.sub(r'Index\s+[:]\s\d+\n', '', r0)
-        r2 = re.sub(r'Username\s+[:]\s', '', r1)
-        r3 = re.sub(r'Assigned IP', '', r2)
-        r4 = re.sub(r'Public IP', '', r3)
-        r5 = re.sub(' +', ' ', r4)
+        resp = con.read()
+        res = str('%s' % (resp)).replace('{"response":["',
+                                         '').replace('"]}', '').replace('\\n', '\n')
+        res0 = re.sub(r'\nGroup Policy\s+[:]\s\w+\s+\w+\s\w+', '', res)
+        res0 = re.sub(r'[:]\s%s' % (GROUP1), '', res0)
+        res1 = re.sub(r'Index\s+[:]\s\d+\n', '', res0)
+        res2 = re.sub(r'Username\s+[:]\s', '', res1)
+        res3 = re.sub(r'Assigned IP', '', res2)
+        res4 = re.sub(r'Public IP', '', res3)
+        response = re.sub(' +', ' ', res4)
         if status_code == 201:
             print "Create was successful"
     except urllib2.HTTPError, err:
-        print "Error received from server. HTTP Status code :"+str(err.code)
+        print "Error received from SERVER. HTTP Status code :"+str(err.code)
         try:
             json_error = json.loads(err.read())
             if json_error:
-                print json.dumps(json_error,sort_keys=True,indent=4, separators=(',', ': '))
+                print json.dumps(json_error, sort_keys=True, indent=4, separators=(',', ': '))
         except ValueError:
             pass
     finally:
-        if f:  f.close()
+        if con:
+            con.close()
+    return response
 
 @restricted
-def srv(bot,update):
-        global r5
-        get_sessions()
-        srv = "" 
-        for line in r5.splitlines():
-            if re.search(r'%s' % (group2), line):
-                srv = srv + "\n" + line
-        count = srv.count('\n')
-        ssrv = '\n'.join(sorted(srv.splitlines()))
-        msg = update.message.reply_text('%s\n*Connected: %d*' % (ssrv,count), parse_mode='markdown')
-
+def srv(bot, update):
+    """Search GROUP2 session and send to tg"""
+    response = get_sessions()
+    ses = ""
+    for line in response.splitlines():
+        if re.search(r'%s' % (GROUP2), line):
+            ses = ses + "\n" + line
+    count = ses.count('\n')
+    ssrv = '\n'.join(sorted(ses.splitlines()))
+    update.message.reply_text('%s\n*Connected: %d*' % (ssrv, count), parse_mode='markdown')
 
 @restricted
-def sessions(bot,update):
-        global r5
-        get_sessions()
-        r5 = '\n'.join(sorted(r5.splitlines()))
-        parts = []
-        while len(r5) > 0:
-            if len(r5) > constants.MAX_MESSAGE_LENGTH:
-                part = r5[:constants.MAX_MESSAGE_LENGTH]
-                first_lnbr = part.rfind('\n')
-                if first_lnbr != -1:
-                    parts.append(part[:first_lnbr])
-                    r5 = r5[first_lnbr:]
-                else:
-                    parts.append(part)
-                    r5 = r5[constants.MAX_MESSAGE_LENGTH:]
+def sessions(bot, update):
+    """Send sessions to tg"""
+    response = get_sessions()
+    response = '\n'.join(sorted(response.splitlines()))
+    parts = []
+    while len(response) > 0:
+        if len(response) > constants.MAX_MESSAGE_LENGTH:
+            part = response[:constants.MAX_MESSAGE_LENGTH]
+            first_lnbr = part.rfind('\n')
+            if first_lnbr != -1:
+                parts.append(part[:first_lnbr])
+                response = response[first_lnbr:]
             else:
-                parts.append(r5)
-                break
-        count = r5.count('\n')
-        msg = None
-        for part in parts:
-            msg = update.message.reply_text('%s\n*Total: %d anyconnect sessions*' % (part,count), parse_mode='markdown')
-            time.sleep(1)
-        return msg  # return only the last message
+                parts.append(part)
+                response = response[constants.MAX_MESSAGE_LENGTH:]
+        else:
+            parts.append(response)
+            break
+    count = response.count('\n')
+    msg = None
+    for part in parts:
+        msg = update.message.reply_text('%s\n*Total: %d anyconnect sessions*' % (part, count),
+                                        parse_mode='markdown')
+        time.sleep(1)
+    return msg  # return only the last message
 
-
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+def error_func(bot, update, error):
+    """error loging function"""
+    LOGGER.warn('Update "%s" caused error "%s"' % (update, error))
 
 def main():
-    updater = Updater(bot_id)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("sessions", sessions))
-    dp.add_handler(CommandHandler("srv", srv))
+    """TG updater function"""
+    updater = Updater(BOT_ID)
+    dsp = updater.dispatcher
+    dsp.add_handler(CommandHandler("start", start))
+    dsp.add_handler(CommandHandler("sessions", sessions))
+    dsp.add_handler(CommandHandler("srv", srv))
     updater.start_polling()
     updater.idle()
 if __name__ == '__main__':
